a9e60bcb4fc2a8e5c4622fa5d0e58cd64c2b3f89
kryptokay <selfwealthplan@gmail.com>
2018-11-01 15:30:05 +0000

    added movie images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import gof from './gof image.jpg';
+import BM from './BM.jpg';
+import BB from './BB.jpg';
+import DN from './DN.jpg';
+import GOT from './GOT.jpg';
+import TW from './TW.jpg';
+import TWD from './TWD.jpg';
 
 class App extends Component {
   render() {
@@ -9,25 +14,39 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to kwallet</h1>
+          <h1 className="App-title">Welcome To Kodflix</h1>
         </header>
         <p className="App-intro">
-          dynamic wallet for the Future Generation
+          All The Best Movies
         </p>
         <div className="container">
-    <div className="item"><h2>Black Mirror</h2></div>
-    <div className="item"><h2>Breaking Bad</h2></div>
-    <div className="item"><h2>Death Note</h2></div>
-    </div>
-    <div className="container">
-    <div className="item"><h2>Gmae Of Thrones</h2></div>
-    <div className="item"><h2>The Walking Dead</h2></div>
-    <div className="item"><h2>The Wire</h2></div>
-    </div>
-        <img class="gof" src={gof} alt="gof" />
+          <div className="item">
+            <img class="BM" src={BM} alt="BM" />
+            <h2>Black Mirror</h2></div>
+          <div className="item">
+            <img class="BB" src={BB} alt="BB" />
+            <h2>Breaking Bad</h2></div>
+          <div className="item">
+            <img class="DN" src={DN} alt="DN" />
+            <h2>Death Note</h2></div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <img class="GOT" src={GOT} alt="GOT" />
+            <h2>Gmae Of Thrones</h2></div>
+          <div className="item">
+            <img class="TW" src={TW} alt="TW" />
+            <h2>The Walking Dead</h2></div>
+          <div className="item">
+            <img class="TWD" src={TWD} alt="TWD" />
+            <h2>The Wire</h2></div>
+        </div>
+        {/* <img class="gof" src={gof} alt="gof" /> */}
+
+
       </div>
     );
-    
+
 
   }
 }
